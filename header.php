b15b38e54f9243286782b4529f36bb960c92c766
slice <slice@zelf.co>
2021-07-13 14:30:22 +0300

    Update header.php
    search

--- header.php
+++ header.php
@@ -20,7 +20,21 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/fonts/font.css">
+	
+	
+	
+	<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W2KTD5X');</script>
+<!-- End Google Tag Manager -->
+	
+	
+	
 	<!-- Global site tag (gtag.js) - Google Analytics -->
+<!--
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-143786884-3"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
@@ -29,13 +43,38 @@ $container = get_theme_mod( 'understrap_container_type' );
 
   gtag('config', 'UA-143786884-3');
 </script>
+-->
 
 
+	<!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1218444878607650');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=1218444878607650&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+
+
+	
+	
 </head>
 
 <body <?php body_class(); ?>>
 <?php do_action( 'wp_body_open' ); ?>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W2KTD5X"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 
   
@@ -96,6 +135,10 @@ $container = get_theme_mod( 'understrap_container_type' );
         
         <li>
           <ul class="side-right-menu">
+          <li>
+           <?php echo do_shortcode('[fibosearch]'); ?>
+            
+          </li>
            <li><?php echo do_shortcode('[woo-currency-switcher]')?></li>
            
            
