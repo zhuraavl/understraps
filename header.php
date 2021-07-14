25640d32af7bb25caa7d02536a72b5c5f5ef7249
slice <slice@zelf.co>
2021-07-14 03:31:45 +0300

    pixel and gtag

--- header.php
+++ header.php
@@ -17,6 +17,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/fonts/font.css">
